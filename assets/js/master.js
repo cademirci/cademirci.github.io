6781f900bb98bc5b75559e8f1f30e5979bc064c7
Caglayan Demirci <demircicaglayan13@gmail.com>
2019-08-07 21:52:48 +0300

    v delta debugging

--- assets/js/master.js
+++ assets/js/master.js
@@ -38,6 +38,9 @@ function scroll() {
         topButton.style.display = "block";
         hiddencp.style.display = "block";
 
+        document.body.scrollTop -= 89;
+        document.documentElement.scrollTop -= 89;
+
         var hashtag = pageHeight / 10,
             numberOfHashtags = seen / hashtag,
             linuxPercent = "[";
