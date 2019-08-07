ee7001cd30296e6a079942eeafe1f7e3034bd23c
Caglayan Demirci <demircicaglayan13@gmail.com>
2019-08-07 21:39:37 +0300

    v delta debugging

--- assets/js/master.js
+++ assets/js/master.js
@@ -32,7 +32,7 @@ function scroll() {
         cp1 = document.getElementById('cp1'),
         cp2 = document.getElementById('cp2');
 
-    if (body.scrollTop > 56 || html.scrollTop > 56) {
+    if (body.scrollTop > 89 || html.scrollTop > 89) {
         constantZone.style.position = "fixed";
         constantZone.style.top = 0;
         topButton.style.display = "block";
@@ -69,4 +69,22 @@ function scroll() {
 function returnTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+function night() {
+    document.body.style.backgroundColor = "#121314";
+    document.body.style.color = "#f2f3f4";
+    document.getElementById('navigation_bar').style.backgroundColor = "132639";
+    document.getElementById('constant_zone').style.backgroundColor = "#222";
+    document.getElementById('night_button').style.display = "none";
+    document.getElementById('day_button').style.display = "block";
+}
+
+function day() {
+    document.body.style.backgroundColor = "#f2f3f4";
+    document.body.style.color = "#222324";
+    document.getElementById('navigation_bar').style.backgroundColor = "#252627";
+    document.getElementById('constant_zone').style.backgroundColor = "#161718";
+    document.getElementById('day_button').style.display = "none";
+    document.getElementById('night_button').style.display = "block";
 }
