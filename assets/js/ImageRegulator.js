6a2662105c30a4e74d47f754cc5c74ed2c49cdee
Caglayan Demirci <demircicaglayan13@gmail.com>
2020-03-27 04:57:36 +0300

    js development

--- assets/js/ImageRegulator.js
+++ assets/js/ImageRegulator.js
@@ -1,24 +1,26 @@
-window.addEventListener('load', () => {
-    regulateImages();
+window.addEventListener('load', regulateImages);
+window.addEventListener('resize', regulateImages);
 
-    function regulateImages() {
-        var windowWidth = window.innerWidth;
-        var maxWidth = Math.floor(windowWidth * 85 / 100);
-        var containerWidth = document.querySelector('.content_container').offsetWidth;
-        if (containerWidth >= 800) {
-            const images = document.querySelectorAll('.reading_area img');
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
+function regulateImages() {
+    var windowWidth = window.innerWidth;
+    var maxWidth = Math.floor(windowWidth * 66 / 100);
+    var container = document.querySelector('.content_container');
+    var containerWidth = container.clientWidth;
+    const images = document.querySelectorAll('.reading_area img');
+    if (containerWidth >= 800) {
+        for (let i = 0; i < images.length; i++) {
+            const image = images[i];
+            if (image.naturalWidth > maxWidth && maxWidth > containerWidth) {
                 image.style.maxWidth = windowWidth;
-                if (image.naturalWidth > maxWidth) {
-                    let margins = Math.floor((maxWidth - containerWidth) / 2);
-                    image.style.marginLeft = -margins;
-                    image.style.marginRight = -margins;
-                    image.style.width = maxWidth;
-                }
+                let margins = Math.floor((maxWidth - containerWidth) / 2);
+                image.style.marginLeft = -margins;
+                image.style.marginRight = -margins;
+                image.style.width = maxWidth;
             }
         }
-
+    } else {
+        for (let i = 0; i < images.length; i++) {
+            images[i].style.maxWidth = "100%";
+        }
     }
-    window.addEventListener('resize', regulateImages);
-})
+}
